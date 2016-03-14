98a268529b9d305a5294ab1740a409cce0f93c06
Errox <ryangroenewold@hotmail.com>
2016-03-14 11:19:42 +0100

    Small things cleaned

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">Zoekresultaten</div>
                     <div class="panel-body">
                         @if (isset($result))
                             @if (empty($result) OR $result[0] == 0)
--- resources/views/topics.blade.php
+++ resources/views/topics.blade.php
@@ -5,10 +5,10 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
+                <div class="panel-heading">Topics</div>
                   <div class="panel-body">
                     <ul>
-                      <table class="table table-hover">
+                      <table class="table table-hover table-striped">
                         <thead>
                           <th>Naam</th>
                           <th>Beschrijving</th>
