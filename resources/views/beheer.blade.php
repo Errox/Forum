21af1978fef8e45c529af4736b330b7832c8ad98
Errox <ryangroenewold@hotmail.com>
2016-03-30 14:24:27 +0200

    Beheer cleanup

--- resources/views/beheer.blade.php
+++ resources/views/beheer.blade.php
@@ -6,15 +6,15 @@
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
                 <div class="panel-body">
-        <div class="container">
+                  <div class="container">
+                  </div>
+                </div>
             </div>
+            <div class="panel panel-default">
+              <div class="panel-body">
+              <h2>Dit is het admin/beheer menu</h2>
               </div>
-          </div>
-          <div class="panel panel-default">
-            <div class="panel-body">
- 
             </div>
-          </div>
 
         </div>
     </div>
