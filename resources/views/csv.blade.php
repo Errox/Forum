22dfc528bb20ed759c47c861ba221280ac9f5a07
Errox <ryangroenewold@hotmail.com>
2016-04-05 13:29:31 +0200

    composer fix

--- resources/views/csv.blade.php
+++ resources/views/csv.blade.php
@@ -8,10 +8,25 @@
                 <div class="panel-heading">Recente leervragen</div>
                   <div class="panel-body centerd">
                     <h1 style="color: red">ALLEEN CSV BESTANDEN</h1>
-                      {{Form::open(array('url' => 'csv', 'files' => true, 'method' => 'post'))}}                      
-                      {{Form::token()}}
-                      {{Form::file('.csv')}}
-                      {{Form::submit('Click Me!')}}
+                      <?php
+                          function readCSV($csvFile){
+  $file_handle = fopen($csvFile, 'r');
+  while (!feof($file_handle) ) {
+    $line_of_text[] = fgetcsv($file_handle, 1024);
+  }
+  fclose($file_handle);
+  return $line_of_text;
+}
+
+
+// Set path to CSV file
+$csvFile = 'test.csv';
+
+$csv = readCSV($csvFile);
+echo '<pre>';
+print_r($csv);
+echo '</pre>';
+                      ?>
                   </div>
                 </div>
             </div>
