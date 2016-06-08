160a23a3afcc0bcaf5d21d699ce05c7481b47dcb
Arcea <rowinvanblokland@hotmail.nl>
2016-06-08 12:29:39 +0200

    changes

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -16,33 +16,37 @@ use Carbon\Carbon;
 class ProfileController extends Controller
 {
   public function __construct()
-    {
+    { // In de construct staat alles wat ingeladen word als de controller aangeroepen word.
         //$this->middleware('auth');
         Carbon::setLocale('nl');
     }
+  // In de Index staat alles wat gebruikt word op de pagina en stuurt je door naar de pagina.  
   public function index(){
+    // Hier worden alle users opgehaald
    	$profile = User::all();
   	if (\Auth::check()){
   	  $user = \Auth::user();
   	}
-
+    // Hier word gekeken of er iemand is ingelogd. Als de user een leeraar is word de user doorgestuurd naar een andere view met meer informatie.
   	if (isset($user)){
   		if ($user->role == 1){
   			return view('gebruikers')->with(compact('profile'));
   		}
   	}
 
-
+    // Hier worden users heen gestuurd die geen leeraar zijn met beperkte informatie.
     return view('profile')->with(compact('profile', 'user'));
 
   }
-
+  // Deze functie pakt de informatie voor een specifiek profiel en stuurt je door naar de juiste blade.php
   public function show($id){
     $profile = User::find($id);
     return view('profileShow')->with(compact('profile', 'id'));
   }
 
+  // Deze functie pakt de informatie om de gebruiker zijn eigen profiel aan te laten passen en stuurt je door naar de juiste blade.php
   public function edit($id){
+    // Hier word eerst gekeken of de ingelogde gebruiker ook de gebruiker is die aangepast gaat worden.
     $profile = User::where('id', '=', $id)->get();
     if (\Auth::check()){
       $user = \Auth::user();
@@ -55,14 +59,17 @@ class ProfileController extends Controller
     }
     return view('profileEdit')->with('profile', $profile);
   }
-
+  // Hier word alles van het profiel geüpdatet met de nieuwe informatie.
     public function update($id){
+      // Hier word eerst alle data van de ingevoerde velden opgehaald
     	$input = Request::all();
     	$update = User::find($id);
+      // Hier word opgehaald welke keuzes de gebruiker heeft gemaakt met betrekking tot zijn privacy.
       $privacy = User_privacy::where('user_id', '=', $id)->get();
     	$update->email = $input['email'];
     	$update->name = $input['username'];
       $update->about =  nl2br($input['about']);
+      // Hier word de nieuwe settings voor zijn email geüpdatet, of het wel of niet getoont mag worden.
       if(empty($input['email_privacy'])){
         $input['email_privacy'] = 0;
       }
